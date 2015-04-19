fde879ff5bf2afca86eff31cc417f728bbb9fdb1
Grzegorz G <tengwar@users.noreply.github.com>
2015-04-19 22:37:45 +0200

    Spaces to tabs in Main

--- src/twofish/Main.java
+++ src/twofish/Main.java
@@ -8,16 +8,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("twofish.fxml"));
-        primaryStage.setTitle("Twofish");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("twofish.fxml"));
+		primaryStage.setTitle("Twofish");
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
+	}
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
